5a52e942a3c6bdf6f9b9746e6fade59e35b47f68
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-17 04:28:00 +0800

    Added getAll todo

--- src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
+++ src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
@@ -1,18 +1,57 @@
 package com.tw.api.unit.test.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tw.api.unit.test.domain.todo.Todo;
 import com.tw.api.unit.test.domain.todo.TodoRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TodoController.class)
 public class TodoControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private TodoRepository todoRepository;
 
-    
+    @Test
+    public void getALL() throws Exception{
+        //given
+        List<Todo> todoList = new ArrayList<>();
+        Todo todo = new Todo(1, "Sample Test", false, 5);
+        todoList.add(todo);
+        when(todoRepository.getAll()).thenReturn(todoList);
+
+        //when
+        ResultActions resultActions = mvc.perform(get("/todos"));
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("Sample Test")))
+                .andExpect(jsonPath("$[0].completed", is(false)))
+                .andExpect(jsonPath("$[0].order", is(5)));
+    }
 
 
 }
